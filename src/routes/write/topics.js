cc0acf6043d23fd9758982cbe36d4e59c856e6a6
Chase Marshall <tdchase34@gmail.com>
2024-10-21 15:05:03 -0400

    Fixes for testing

--- src/routes/write/topics.js
+++ src/routes/write/topics.js
@@ -49,8 +49,8 @@ module.exports = function () {
 	setupApiRoute(router, 'delete', '/:tid/read', [...middlewares, middleware.assert.topic], controllers.write.topics.markUnread);
 	setupApiRoute(router, 'put', '/:tid/bump', [...middlewares, middleware.assert.topic], controllers.write.topics.bump);
 
-	setupApiRoute(router, 'put', '/:tid/solve', [...middlewares, middleware.assert.topic], controllers.write.topics.solve);
-	setupApiRoute(router, 'put', '/:tid/unsolve', [...middlewares, middleware.assert.topic], controllers.write.topics.unsolve);
+	// setupApiRoute(router, 'put', '/:tid/solve', [...middlewares, middleware.assert.topic], controllers.write.topics.solve);
+	// setupApiRoute(router, 'put', '/:tid/unsolve', [...middlewares, middleware.assert.topic], controllers.write.topics.unsolve);
 
 	return router;
 };
